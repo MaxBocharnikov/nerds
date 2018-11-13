92422a06301f2b4fb4770241a643bb4f76ef362c
MaxBocharnikov <Bocharnikov3009@gmail.com>
2018-11-13 20:53:42 +0600

    Added simple JS to slider on index page

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@ var writeUsName = writeUsModal.querySelector(".modal-name");
 var writeUsEmail = writeUsModal.querySelector(".modal-email");
 var writeUsMessage = writeUsModal.querySelector(".modal-message");
 
+var control1 = document.querySelector("#control-1");
+var control2 = document.querySelector("#control-2");
+var control3 = document.querySelector("#control-3");
+
+var slide1 = document.querySelector(".slide:nth-child(1)");
+var slide2 = document.querySelector(".slide:nth-child(2)");
+var slide3 = document.querySelector(".slide:nth-child(3)");
+
 mapButton.addEventListener("click", function(e){
 	e.preventDefault();
 
@@ -110,6 +118,41 @@ writeUsSend.addEventListener("click", function(e){
 		localStorage.setItem("name", writeUsName.value);
 	}
 
+});
 
+
+control1.addEventListener("click", function(){
+	if(control2.classList.contains("active")) control2.classList.remove("active");
+	if(control3.classList.contains("active")) control3.classList.remove("active");
+	control1.classList.add("active");
+
+	if(slide2.classList.contains("slide-show")) slide2.classList.remove("slide-show");
+	if(slide3.classList.contains("slide-show")) slide3.classList.remove("slide-show");
+	slide1.classList.add("slide-show");
 
 });
+
+
+control2.addEventListener("click", function(){
+	if(control1.classList.contains("active")) control1.classList.remove("active");
+	if(control3.classList.contains("active")) control3.classList.remove("active");
+	control2.classList.add("active");
+
+	if(slide1.classList.contains("slide-show")) slide1.classList.remove("slide-show");
+	if(slide3.classList.contains("slide-show")) slide3.classList.remove("slide-show");
+	slide2.classList.add("slide-show");
+});
+
+
+control3.addEventListener("click", function(){
+	if(control1.classList.contains("active")) control1.classList.remove("active");
+	if(control2.classList.contains("active")) control2.classList.remove("active");
+	control3.classList.add("active");
+
+	if(slide1.classList.contains("slide-show")) slide1.classList.remove("slide-show");
+	if(slide2.classList.contains("slide-show")) slide2.classList.remove("slide-show");
+	slide3.classList.add("slide-show");
+});
+
+
+
